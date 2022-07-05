09d5596891ff884a8c4ed1bbd55809dedff8e269
Aniket-1211 <aniket66patel@gmail.com>
2022-07-05 15:14:35 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
   return (
     <>
     <Navbar/> 
-    {/* <Banner/> */}
-    {/* <MovieList/> */}
-    <Fav/>
+    <Banner/>
+    <MovieList/>
+    {/* <Fav/> */}
     </>
   );
 }
